d6e55bae11cebedc196f0a596ad30211cfc76c3b
Ion M <ion@Ions-MacBook-Pro.local>
2018-05-03 17:05:44 -0400

    Add Pitch Perfect to git

---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Ion M on 4/26/18.
+//  Copyright © 2018 Ion M. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //stopRecordingButton.isEnabled = false
+        stopRecordingButton.isHidden = true
+    }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        configureUI(recordingActive: true, stopRecordingActive: false, label: "Tap to finish recording")
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        configureUI(recordingActive: false, stopRecordingActive: true, label: "Tap to start recording")
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    // Show/hide the record and stop recording buttons as needed. Change the label accordingly
+    func configureUI(recordingActive: Bool, stopRecordingActive: Bool, label: String) {
+        recordButton.isHidden = recordingActive
+        stopRecordingButton.isHidden = stopRecordingActive
+        recordingLabel.text = label
+    }
+    
+    // MARK: - Audio Recorder Delegate
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        // if flag = true then performSegue, otherwise print the message
+        flag ? performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url) : print("Recording failed")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+}
+
